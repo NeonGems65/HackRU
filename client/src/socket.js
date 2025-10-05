e11e72384258381518d7768c3917e22d76a045c9
NeonGems65 <ishaankumarb65@gmail.com>
2025-10-04 21:57:13 -0400

    yupippeee socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,14 +1,20 @@
 // client/src/socket.js
 import { io } from "socket.io-client";
 
-// ✅ LOCAL development
-// const socket = io(`http://${window.location.hostname}:3000`, {
+const BACKEND_URL = import.meta.env.PROD
+  ? "https://math-battle-backend.onrender.com"
+  : `http://${window.location.hostname}:3000`;
+
+//✅ LOCAL development
+const socket = io(BACKEND_URL, {
+  transports: ["websocket"],
+    withCredentials: true,
+   // force websocket (fixes xhr poll errors)
+});
+
+// const socket = io(`74.220.49.0/24`, {
 //   transports: ["websocket"], // force websocket (fixes xhr poll errors)
 // });
-
-const socket = io(`74.220.49.0/24`, {
-  transports: ["websocket"], // force websocket (fixes xhr poll errors)
-});
 
 // When leaving the page or refreshing
 window.addEventListener("beforeunload", () => {
